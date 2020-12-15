1570190c3442f8848725fb03ac62602d88fe44ac
ertrii <ertriilive@gmail.com>
2020-12-15 02:50:51 -0500

    details design

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import FilterGraphics from '../Filters/FilterGraphics'
 import { PropsLayout } from './layout.interface'
 import Styles from './layout.module.css'
